338168aaf84016c7c58e8f82b6aaa8b77d96f925
Ifrim Alexandra <157494822+ale23yfm@users.noreply.github.com>
2025-12-11 19:17:11 +0200

    renamed the function

--- lab8/pb2/module.asm
+++ lab8/pb2/module.asm
@@ -1,7 +1,7 @@
 bits 32 ; assembling for the 32 bits architecture
 
 ; declare the EntryPoint (a label defining the very first instruction of the program)
-global read        
+global printing        
 
 ; declare external functions needed by our program
 extern printf     
@@ -11,7 +11,7 @@ segment data use32 class=data
     format db "%s", 0
 
 segment code use32 class=code
-    read:
+    printing:
         ;printf(format, text)
         push dword [esp+4]
         push dword format
